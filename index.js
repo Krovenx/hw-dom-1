d13fd34ebc4d6aacc0feba0cda560f1695152320
Krovenx <Krovenx2@mail.ru>
2025-10-10 12:42:34 +0500

    Добавление и распределение js по  модульно

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-export {renderComments} from './modules/renderComments.js';
+import { commentsData } from './modules/commentsData.js';
+import { renderComments } from './modules/renderComments.js';
+import './modules/addListener.js';
+
+
+
 renderComments();
